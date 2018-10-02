e06bb739e83e0e50b4112c62a91257f51e06e4f7
awgualbert <allanwesleyoliveira@gmail.com>
2018-10-02 16:35:30 -0300

    teste

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -10,26 +10,36 @@ class NegociacaoController {
 		this. _inputData = $('#data');
 		this._inputQuantidade = $('#quantidade');
 		this._inputValor = $('#valor');
+		this._negociacoes = new Negociacoes();
 	}
 
 	adiciona(event) {
 		// cancelando a submissão do formulário
 		event.preventDefault();
+		this._negociacoes.adiciona(this._criaNegociacao());
 
-		//Chamando o método estático
-			let negociacao = new Negociacao(
+		// será que conseguimos apagar a lista?
+		this._negociacoes.paraArray().length = 0;
+
+
+		console.log(this._negociacoes.paraArray());
+		this._limpaFormulario();
+	}
+
+	_limpaFormulario(){
+		this._inputData.value = '';
+		this._inputQuantidade.value = 1;
+		this._inputValor.value = 0.0;
+		this._inputData.focus();
+	}
+
+	_criaNegociacao() {
+		//retorna uma instância de nogociação
+		return new Negociacao(
 			DateConverter.paraData(this._inputData.value),
 			parseInt(this._inputQuantidade.value),
 			parseFloat(this._inputValor.value)
 		);
-
-		console.log(negociacao.data);
-
-		//Chamando o método estático
-		let diaMesAno = DateConverter.paraTexto(negociacao.data);
-		console.log(diaMesAno);
-
-		
 	}
 }
 
--- client/app/ui/converters/DateConverter.js
+++ client/app/ui/converters/DateConverter.js
@@ -9,9 +9,11 @@ class DateConverter {
 	}
 
 	static paraData(texto) {
-		if(!/^\d{4}-\d{2}$/.test(texto)) throw new Error('Deve estar no formato aaaa-mm-dd');
-			throw new Error('Deve estar no formato aaaa-mm-dd');
-		
-		return new Date(...texto.split('-').map((item,indice) => item - indice % 2));
+		if(!/^\d{4}-\d{2}-\d{2}$/.test(texto))
+			throw new Error('Deve	estar	no	formato	aaaa-mm-dd');
+	
+	return new Date(...texto.split('-').map((item,indice)	=>	
+	item	-	indice	%	2));
+	
 	}
 }
